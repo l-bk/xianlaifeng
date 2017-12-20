75adbc1ec9fcd4048701c8ab45004fea7b7f62dc
l-bk <644523139@qq.com>
2017-12-20 21:23:51 +0800

    图片完善。

--- jeesite/src/main/java/com/thinkgem/jeesite/modules/ptj/service/XlfPartTimeJobService.java
+++ jeesite/src/main/java/com/thinkgem/jeesite/modules/ptj/service/XlfPartTimeJobService.java
@@ -30,32 +30,42 @@ import com.thinkgem.jeesite.modules.ptj.dao.XlfPartTimeJobDao;
 @Service
 @Transactional(readOnly = true)
 public class XlfPartTimeJobService extends CrudService<XlfPartTimeJobDao, XlfPartTimeJob> {
-	
-	
+
+
 	@Autowired
 	private XlfAreaDao areaDao;
-	
+
 	@Autowired 
 	private XlfPartTimeJobDao tpjDao;
 
 	public XlfPartTimeJob get(String id) {
 		return super.get(id);
 	}
-	
+
 	public List<XlfPartTimeJob> findList(XlfPartTimeJob xlfPartTimeJob) {
 		return super.findList(xlfPartTimeJob);
 	}
-	
+
 	public Page<XlfPartTimeJob> findPage(Page<XlfPartTimeJob> page, XlfPartTimeJob xlfPartTimeJob) {
 		return super.findPage(page, xlfPartTimeJob);
 	}
-	
+
 	@Transactional(readOnly = false)
 	public void save(XlfPartTimeJob xlfPartTimeJob) {
 		if(xlfPartTimeJob.getJobId() == null || "".equals(xlfPartTimeJob.getJobId())) { //添加信息
 			xlfPartTimeJob.setReleaseTime(new Date());
 			xlfPartTimeJob.setAuditStatus("1");//管理员录入的东西默认审核通过
+
 		}
+
+		if(StringUtils.isNotBlank(xlfPartTimeJob.getJobContent())) {
+			String details=Encodes.unescapeHtml(xlfPartTimeJob.getJobContent());
+			if(details.contains("<img")) {
+				details=details.replace("src=\"/", "src=\\\"https://www.xianlaifeng.com/");
+				xlfPartTimeJob.setJobContent(details);
+			}
+		}
+
 		xlfPartTimeJob.setId(xlfPartTimeJob.getJobId());//id用来判断是添加还是修改
 		if(StringUtils.isNotBlank(xlfPartTimeJob.getJobContent())) {//将内容转码为html标签
 			xlfPartTimeJob.setJobContent(Encodes.unescapeHtml(xlfPartTimeJob.getJobContent()));
@@ -65,57 +75,57 @@ public class XlfPartTimeJobService extends CrudService<XlfPartTimeJobDao, XlfPar
 				&& null != xlfPartTimeJob.getEndWorkDate() && !"".equals(xlfPartTimeJob.getEndWorkDate())) {
 			int days = TimeUtils.differentDaysByMillisecond(xlfPartTimeJob.getEndWorkDate(), xlfPartTimeJob.getStartWorkDate());
 			if(days < 5){
-	               xlfPartTimeJob.setTimeType("1");
-	               Calendar calendar=Calendar.getInstance();
-	               calendar.setTime(xlfPartTimeJob.getStartWorkDate());
-	               Date date= xlfPartTimeJob.getStartWorkDate();
-	               int result=0;
-	               for(int i=0;i<days;i++){
-	                   try {
-	                       result=TimeUtils.dayForWeek(date);
-	                       if(result == 7 || result == 6){
-	                           xlfPartTimeJob.setTimeType("2");
-	                           break;
-	                       }
-	                      calendar.add(Calendar.DAY_OF_MONTH,1);
-	                       date=calendar.getTime();
+				xlfPartTimeJob.setTimeType("1");
+				Calendar calendar=Calendar.getInstance();
+				calendar.setTime(xlfPartTimeJob.getStartWorkDate());
+				Date date= xlfPartTimeJob.getStartWorkDate();
+				int result=0;
+				for(int i=0;i<days;i++){
+					try {
+						result=TimeUtils.dayForWeek(date);
+						if(result == 7 || result == 6){
+							xlfPartTimeJob.setTimeType("2");
+							break;
+						}
+						calendar.add(Calendar.DAY_OF_MONTH,1);
+						date=calendar.getTime();
 
-	                   } catch (Exception e) {
-	                       e.printStackTrace();
-	                   }
-	               }
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 
 
-	            }else{//大于五天，表示存在周末
-	               if(days <15){
-	                    xlfPartTimeJob.setTimeType("2");//小于15天大于5天，表示短期包含周末
-	               }else{
-	                    xlfPartTimeJob.setTimeType("3");//长期，大于15天
-	               }
-	           }
-
-	        }
-			XlfArea area= new XlfArea();
-			area.setProvince(xlfPartTimeJob.getWorkProvince());
-			area.setCity(xlfPartTimeJob.getWorkCity());
-			area.setDistrict(xlfPartTimeJob.getWorkDistrict());
-			area = areaDao.selectByArea(area);
-			if(area != null) {
-				xlfPartTimeJob.setAreaId(area.getAreaId());
+			}else{//大于五天，表示存在周末
+				if(days <15){
+					xlfPartTimeJob.setTimeType("2");//小于15天大于5天，表示短期包含周末
+				}else{
+					xlfPartTimeJob.setTimeType("3");//长期，大于15天
+				}
 			}
+
+		}
+		XlfArea area= new XlfArea();
+		area.setProvince(xlfPartTimeJob.getWorkProvince());
+		area.setCity(xlfPartTimeJob.getWorkCity());
+		area.setDistrict(xlfPartTimeJob.getWorkDistrict());
+		area = areaDao.selectByArea(area);
+		if(area != null) {
+			xlfPartTimeJob.setAreaId(area.getAreaId());
+		}
 		xlfPartTimeJob.setUserId(10001);
 		super.save(xlfPartTimeJob);
 	}
-	
+
 	@Transactional(readOnly = false)
 	public void delete(XlfPartTimeJob xlfPartTimeJob) {
 		super.delete(xlfPartTimeJob);
 	}
-	
-	
+
+
 	@Transactional(readOnly = false)
 	public int updateStatus(XlfPartTimeJob xlfPartTimeJob) {
 		return tpjDao. updateStatus( xlfPartTimeJob);
 	}
-	
+
 }
